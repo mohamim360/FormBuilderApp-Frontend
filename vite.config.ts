f5b4a7d36d7356875409cff6ef5a406ae88ddafc
mohamim360 <mohamim360@gmail.com>
2025-07-04 18:42:02 +0600

    Add Dockerfile and docker-compose configuration for frontend and backend services

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
+  server: {
+    host: true,
+    port: 5173,
     proxy: {
       '/api/odoo': {
         target: 'https://formbuilder1.odoo.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/odoo/, '/api')
-      }
-    }
-  }
+        rewrite: (path) => path.replace(/^\/api\/odoo/, '/api'),
+      },
+    },
+  },
 })
